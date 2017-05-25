741ca84ae169d6be47e3e5fe70933a1223a5d7c8
“DFGriffis” <david.griffis@ix.netcom.com>
2017-05-25 05:50:04 -0400

    adding files to git repo

---
+++ gym_eval.py
@@ -0,0 +1,102 @@
+from __future__ import division
+import math
+import argparse
+import os
+import sys
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from envs import atari_env, read_config
+from model import A3Clstm, normalized_columns_initializer
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import time
+import gym
+import my_optim
+import logging
+
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s : %(message)s')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler)
+
+
+parser = argparse.ArgumentParser(description='A3C')
+parser.add_argument('--env-name', default='Pong-v0', metavar='ENV',
+                    help='environment to train on (default: Pong-v0)')
+parser.add_argument('--env-config', default='config.json', metavar='EC',
+                    help='environment to crop and resize info (default: config.json)')
+parser.add_argument('--num-episodes', type=int, default=100, metavar='NE',
+                    help='how many episodes in evaluation (default: 100)')
+parser.add_argument('--load-model-dir', default='trained_models/', metavar='LMD',
+                    help='folder to load trained models from')
+parser.add_argument('--log-dir', default='logs/', metavar='LG',
+                    help='folder to save logs')
+args = parser.parse_args()
+
+setup_json = read_config(args.env_config)
+env_conf = setup_json[args.env_name]
+torch.set_default_tensor_type('torch.FloatTensor')
+
+saved_state = torch.load('{0}{1}.dat'.format(args.load_model_dir, args.env_name),
+                         map_location=lambda storage, loc: storage)
+
+done = True
+
+log = {}
+setup_logger('{}_mon_log'.format(args.env_name),
+             r'{0}{1}_mon_log'.format(args.log_dir, args.env_name))
+log['{}_mon_log'.format(args.env_name)] = logging.getLogger(
+    '{}_mon_log'.format(args.env_name))
+
+
+env = atari_env("{}".format(args.env_name), env_conf)
+
+model = A3Clstm(env.observation_space.shape[0], env.action_space)
+
+model.eval()
+
+env = gym.wrappers.Monitor(env, "{}_monitor".format(args.env_name), force=True)
+num_tests = 0
+reward_total_sum = 0
+for i_episode in range(args.num_episodes):
+    state = env.reset()
+    episode_length = 0
+    reward_sum = 0
+    while True:
+
+        # Sync with the shared model
+        if done:
+            model.load_state_dict(saved_state)
+            cx = Variable(torch.zeros(1, 512), volatile=True)
+            hx = Variable(torch.zeros(1, 512), volatile=True)
+        else:
+            cx = Variable(cx.data, volatile=True)
+            hx = Variable(hx.data, volatile=True)
+
+        state = torch.from_numpy(state).float()
+        value, logit, (hx, cx) = model(
+            (Variable(state.unsqueeze(0), volatile=True), (hx, cx)))
+        prob = F.softmax(logit)
+        action = prob.max(1)[1].data.numpy()
+        state, reward, done, _ = env.step(action[0, 0])
+        episode_length += 1
+        reward_sum += reward
+        done = done or episode_length >= 10000
+        if done:
+            num_tests += 1
+            reward_total_sum += reward_sum
+            reward_mean = reward_total_sum / num_tests
+            log['{}_mon_log'.format(args.env_name)].info(
+                "reward sum: {0}, reward mean: {1:.4f}".format(reward_sum, reward_mean))
+
+            break
---
+++ model.py
@@ -0,0 +1,74 @@
+import math
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+def normalized_columns_initializer(weights, std=1.0):
+    out = torch.randn(weights.size())
+    out *= std / torch.sqrt(out.pow(2).sum(1).expand_as(out))
+    return out
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = np.prod(weight_shape[1:4])
+        fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
+    elif classname.find('Linear') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = weight_shape[1]
+        fan_out = weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
+
+
+class A3Clstm(torch.nn.Module):
+    def __init__(self, num_inputs, action_space):
+        super(A3Clstm, self).__init__()
+        self.conv1 = nn.Conv2d(num_inputs, 32, 5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(32, 32, 5, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 4, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+
+        self.lstm = nn.LSTMCell(1024, 512)
+        num_outputs = action_space.n
+        self.critic_linear = nn.Linear(512, 1)
+        self.actor_linear = nn.Linear(512, num_outputs)
+
+        self.apply(weights_init)
+        self.actor_linear.weight.data = normalized_columns_initializer(
+            self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(
+            self.critic_linear.weight.data, 1.0)
+        self.critic_linear.bias.data.fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+
+        self.train()
+
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        x = F.relu(F.max_pool2d(self.conv1(inputs), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv3(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv4(x), kernel_size=2, stride=2))
+
+        x = x.view(x.size(0), -1)
+
+        hx, cx = self.lstm(x, (hx, cx))
+
+        x = hx
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
