72a855822366d4b427327f9646d723b6d0ce753c
David Griffis <david.griffis@ix.netcom.com>
2018-04-28 09:50:32 -0400

    update

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ def ensure_shared_grads(model, shared_model, gpu=False):
     for param, shared_param in zip(model.parameters(), shared_model.parameters()):
         if shared_param.grad is not None and not gpu:
             return
-        if not gpu:
+        elif not gpu:
             shared_param._grad = param.grad
         else:
             shared_param._grad = param.grad.cpu()
