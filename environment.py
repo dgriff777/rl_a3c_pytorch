cc412cafb632dfd0fd4c2a0ba48aef650f856fac
David Griffis <david.griffis@ix.netcom.com>
2018-05-10 23:59:11 -0400

    fix

--- environment.py
+++ environment.py
@@ -14,8 +14,11 @@ def atari_env(env_id, env_conf, args):
     env = gym.make(env_id)
     if 'NoFrameskip' in env_id:
         assert 'NoFrameskip' in env.spec.id
+        env._max_episode_steps = args.max_episode_length * args.skip_rate
         env = NoopResetEnv(env, noop_max=30)
         env = MaxAndSkipEnv(env, skip=args.skip_rate)
+    else:
+        env._max_episode_steps = args.max_episode_length
     env = EpisodicLifeEnv(env)
     if 'FIRE' in env.unwrapped.get_action_meanings():
         env = FireResetEnv(env)
