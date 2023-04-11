e03007c88bb53dddd556f58b700fec51ff8de354
David Griffis <24666148+dgriff777@users.noreply.github.com>
2023-04-11 17:18:20 -0400

    update to model

--- model.py
+++ model.py
@@ -10,30 +10,31 @@ class A3Clstm(torch.nn.Module):
         super(A3Clstm, self).__init__()
         self.hidden_size = args.hidden_size
         self.conv1 = nn.Conv2d(num_inputs, 32, 5, stride=1, padding=2)
-        self.maxp1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 32, 5, stride=1, padding=1)
-        self.maxp2 = nn.MaxPool2d(2, 2)
         self.conv3 = nn.Conv2d(32, 64, 4, stride=1, padding=1)
-        self.maxp3 = nn.MaxPool2d(2, 2)
         self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
-        self.maxp4 = nn.MaxPool2d(2, 2)
 
         self.lstm = nn.LSTMCell(1024, self.hidden_size)
         num_outputs = action_space.n
         self.critic_linear = nn.Linear(512, 1)
         self.actor_linear = nn.Linear(512, num_outputs)
 
-        self.apply(weights_init)
-        relu_gain = nn.init.calculate_gain('relu')
+        relu_gain = nn.init.calculate_gain("relu")
         self.conv1.weight.data.mul_(relu_gain)
+        self.conv1.bias.data.fill_(0)
         self.conv2.weight.data.mul_(relu_gain)
+        self.conv2.bias.data.fill_(0)
         self.conv3.weight.data.mul_(relu_gain)
+        self.conv3.bias.data.fill_(0)
         self.conv4.weight.data.mul_(relu_gain)
+        self.conv4.bias.data.fill_(0)
         self.actor_linear.weight.data = norm_col_init(
-            self.actor_linear.weight.data, 0.01)
+            self.actor_linear.weight.data, 0.01
+        )
         self.actor_linear.bias.data.fill_(0)
         self.critic_linear.weight.data = norm_col_init(
-            self.critic_linear.weight.data, 1.0)
+            self.critic_linear.weight.data, 1.0
+        )
         self.critic_linear.bias.data.fill_(0)
 
         for name, p in self.named_parameters():
@@ -53,10 +54,10 @@ class A3Clstm(torch.nn.Module):
         self.train()
 
     def forward(self, inputs, hx, cx):
-        x = F.relu(self.maxp1(self.conv1(inputs)))
-        x = F.relu(self.maxp2(self.conv2(x)))
-        x = F.relu(self.maxp3(self.conv3(x)))
-        x = F.relu(self.maxp4(self.conv4(x)))
+        x = F.relu(F.max_pool2d(self.conv1(inputs), 2, 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2, 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2, 2))
+        x = F.relu(F.max_pool2d(self.conv4(x), 2, 2))
 
         x = x.view(x.size(0), -1)
 
